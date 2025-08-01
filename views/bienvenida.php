8bf155b9ed603f54ed74594d6b779c645ea972f3
GinoX02 <Ginovalenz@gmail.com>
2025-08-01 09:02:09 -0400

    Agregar página principal de bienvenida con enlaces

---
+++ views/bienvenida.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <h1>Bienvenido al Visor de Usuarios</h1>
+    <p>Este sistema te permite buscar y visualizar usuarios registrados.</p>
+
+    <div class="acciones">
+        <a href="buscar.php" class="boton">🔍 Buscar Usuarios</a>
+        <a href="registro.php" class="boton">📝 Registrarse</a>
+    </div>
+</div>
